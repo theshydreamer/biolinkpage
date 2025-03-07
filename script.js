4f63fe4a44e263b8517834a2dd96052927061ac6
John <johnharrybeato@gmail.com>
2025-03-07 17:43:04 +0800

    updated links

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function toggleTheme() {
   localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
 }
 
-// Load saved theme on refresh
 window.onload = () => {
   if (localStorage.getItem("theme") === "dark") {
       document.body.classList.add("dark-mode");
